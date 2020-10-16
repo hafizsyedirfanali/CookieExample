f47157400bf392b55613331b9581771f7425eb7a
hafizsyedirfanali <hafizsyedirfanali@gmail.com>
2020-10-16 22:23:49 +0530

    Add project files.

---
+++ CookieExample/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CookieExample.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace CookieExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            string Key = "MySessionCookie";
+            string Value = "MyCookieValue";
+            CookieOptions option = new CookieOptions();
+            
+            Response.Cookies.Append(Key, Value,option);
+            return View("Index");
+        }
+    }
+}
